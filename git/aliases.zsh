83dc698911e002afbf333e626e98ea3cfcb08560
Robert Böhnke <robb@soundcloud.com>
2012-04-11 12:10:33 +0200

    Adding fast g function by @hannestyden

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,12 @@
-alias g='git'
 alias gl='git log --pretty=oneline --decorate'
 alias gs='git status'
+
+g () {
+  if [ $# -eq 0 ]
+  then
+    git status -sb
+  else
+    git $*
+  fi
+}
+compdef g=git
