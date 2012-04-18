6b243f6812d0fd55aa5f8e3b75d8e9db2079a928
Robert Böhnke <robb@soundcloud.com>
2012-04-18 15:57:15 +0200

    Improving auto-completion

---
+++ zsh/completion.zsh
@@ -0,0 +1,14 @@
+# Show completion menu on succesive tab press
+setopt auto_menu
+setopt complete_in_word
+setopt always_to_end
+
+# Enable menu selection
+zstyle ':completion:*' menu select
+
+# Enable case insensitive and backwards completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Completion caching
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path ~/.zsh/cache/$HOST
