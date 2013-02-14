8bc2b6d9e4efeb741058473c72801d94256873f3
Markus Emrich <Markus.Emrich@gmail.com>
2013-02-14 11:54:26 +0100

    updated create gitrepo function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,8 +13,20 @@ function gitrepo {
     repo=$1
   fi
 
-  echo "Creating Repository '$repo'"
-  echo "https://api.github.com/user/repos -d '{\"name\":\"$repo\"}'"
+  echo "Creating Repository '${repo}'"
+  echo "https://api.github.com/user/repos -d '{\"name\":\"${repo}\"}'"
 
   curl -u "jaydee3:${pw}" https://api.github.com/user/repos -d "{\"name\":\"${repo}\"}"
+
+  echo "\n\nRepository '${repo}' created!"
+
+  echo "Init local repository in current directory? (y/n)"
+  read localRepo
+
+  if [ "$localRepo" = "y" ] || [ "$localRepo" = "yes" ];
+  then
+    echo "Initalizing local repository..."
+    git init
+    git remote add origin git@github.com:jaydee3/${repo}
+  fi
 }
