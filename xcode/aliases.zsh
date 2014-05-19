021555adf766e5ca728531b5391540f6a50cca3b
Robert Böhnke <robb@robb.is>
2014-05-19 23:52:14 +0200

    Open Xcode files from the command line

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,4 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
+
+alias -s xcworkspace=open
+alias -s xcodeproj=open
