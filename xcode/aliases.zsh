ea4170de00cd77a69139fe7664d84ad8cd9102f8
Robert Böhnke <robb@robb.is>
2017-06-12 23:58:31 -0700

    Remove zsh

--- xcode/aliases.zsh
+++
@@ -1,5 +0,0 @@
-alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
-
-alias -s xcworkspace=open
-alias -s xcodeproj=open
-alias -s playground=open
