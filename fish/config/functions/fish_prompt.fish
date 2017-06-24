0c0cb158958af33c507231ea937071f9e31a5312
Robert Böhnke <robb@robb.is>
2017-06-23 23:29:30 -0700

    Improve fish prompt

--- fish/config/functions/fish_prompt.fish
+++ fish/config/functions/fish_prompt.fish
@@ -1,13 +1,5 @@
-set -x fish_color_autosuggestion -i blue
-set -x fish_color_command -i black
-set -x fish_color_error -i red
-set -x fish_color_match black
-set -x fish_color_operator -i black
-set -x fish_color_param -i black
-set -x fish_color_valid_path 'F80 --underline'
-
 function fish_prompt
-    if test $status -eq 0
+	if test $status -eq 0
         set exit_color normal
     else
         set exit_color red
@@ -19,16 +11,38 @@ function fish_prompt
 
     # Test if we're inside a git directory
     if git rev-parse --git-dir > /dev/null 2>&1
-        set current_branch (git symbolic-ref --quiet --short HEAD | tr -d '[:space:]')
+        # Print a git prompt
+        begin
+            # Print the current branch name or sha if we're in detached HEAD.
+            set current_branch (git symbolic-ref --quiet --short HEAD | tr -d '[:space:]')
 
-        if test -n "$current_branch"
-            set branch_state (printf " at %s%s%s" (set_color -o black) \
-                                                  $current_branch \
-                                                  (set_color normal))
-        else
-            set branch_state (printf " at %s%s%s" (set_color -o red) \
-                                                  (git rev-parse --short HEAD ^ /dev/null) \
-                                                  (set_color normal))
+            if test -n "$current_branch"
+                set branch_state (printf " at %s%s%s" (set_color -o black) \
+                                                      $current_branch \
+                                                      (set_color normal))
+            else
+                set branch_state (printf " at %s%s%s" (set_color -o red) \
+                                                      (git rev-parse --short HEAD ^ /dev/null) \
+                                                      (set_color normal))
+            end
+
+            # Print how many commits we're ahead or behind upstream
+            set remote_ref (git for-each-ref --format='%(upstream:short)' (git symbolic-ref -q HEAD))
+            set commits_different_remotely (git rev-list --left-right $remote_ref...HEAD)
+
+            set ahead  (echo $commits_different_remotely | grep '>' | wc -l | tr -d ' ' ^ /dev/null)
+            set behind (echo $commits_different_remotely | grep '<' | wc -l | tr -d ' ' ^ /dev/null)
+
+            if test $ahead -gt 0 -a $behind -gt 0
+                set ahead_behind_state (printf ", %s↑%s%s, %s↓%s%s" (set_color -o black) $ahead  (set_color normal) \
+                                                                    (set_color -o black) $behind (set_color normal))
+            else if test $ahead -gt 0
+                set ahead_behind_state (printf ", %s↑%s%s" (set_color -o black) $ahead (set_color normal))
+            else if test $behind -gt 0
+                set ahead_behind_state (printf ", %s↓%s%s" (set_color -o black) $behind (set_color normal))
+            end
+
+            set git_prompt "$branch_state""$ahead_behind_state"
         end
 
         # Separator
@@ -50,29 +64,9 @@ function fish_prompt
                 set separator '–'
             end
         end
-
-        # git prompt
-        begin
-            set remote_ref (git for-each-ref --format='%(upstream:short)' (git symbolic-ref -q HEAD))
-            set commits_different_remotely (git rev-list --left-right $remote_ref...HEAD)
-
-            set ahead  (echo $commits_different_remotely | grep '>' | wc -l | tr -d ' ' ^ /dev/null)
-            set behind (echo $commits_different_remotely | grep '<' | wc -l | tr -d ' ' ^ /dev/null)
-
-            if test $ahead -gt 0 -a $behind -gt 0
-                set ahead_behind_state (printf ", %s↑%s%s, %s↓%s%s" (set_color -o black) $ahead  (set_color normal) \
-                                                                    (set_color -o black) $behind (set_color normal))
-            else if test $ahead -gt 0
-                set ahead_behind_state (printf ", %s↑%s%s" (set_color -o black) $ahead (set_color normal))
-            else if test $behind -gt 0
-                set ahead_behind_state (printf ", %s↓%s%s" (set_color -o black) $behind (set_color normal))
-            end
-
-            set git_prompt "$branch_state""$ahead_behind_state"
-        end
     else
-        set separator '–'
         set git_prompt ''
+        set separator '–'
     end
 
     printf '%s¶%s %s%s %s ' (set_color $exit_color) \
