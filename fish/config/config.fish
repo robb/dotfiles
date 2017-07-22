a24728843c6f1ab85306a7b7826e58aba887ebab
Robert Böhnke <robb@robb.is>
2017-07-21 21:15:07 -0700

    Set locale explicitly

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -1,6 +1,11 @@
 # iTerm2 integration
 source ~/.config/fish/iterm2_shell_integration.fish
 
+# Locale
+set -x LANGUAGE "en_US.UTF-8"
+set -x LC_ALL "en_US.UTF-8"
+set -x LANG "en_US.UTF-8"
+
 # Homebrew
 set PATH "/usr/local/sbin" $PATH
 
