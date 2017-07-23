aa2e4c4cbba116ae927c8565cb7af8b58d8ab0bc
Robert Böhnke <robb@robb.is>
2017-07-22 22:57:25 -0700

    Make `gl` an abbreviation for `g log`

--- fish/config/abbreviations.fish
+++ fish/config/abbreviations.fish
@@ -1,4 +1,5 @@
 if status --is-interactive
     set -g fish_user_abbreviations
-    abbr --add gl 'git log --pretty=oneline --decorate --abbrev-commit'
+
+    abbr --add gl 'g log --pretty=oneline --decorate --abbrev-commit'
 end
