2381a7907dd1efea6f4bc4474dfc4e69dce26dc2
Robb Böhnke <robb@robb.is>
2018-04-13 14:44:02 -0700

    Make gl wrap at the terminal boundary

--- fish/config/abbreviations.fish
+++ fish/config/abbreviations.fish
@@ -3,6 +3,6 @@ if status --is-interactive
 
     abbr --add .xco 'open *.xcodeproj'
     abbr --add .xcw 'open *.xcworkspace'
-    abbr --add gl   'g log --pretty=oneline --decorate --abbrev-commit'
+    abbr --add gl   'g log --pretty=oneline --decorate --abbrev-commit --format="%C(yellow)%h%C(reset)%w("(math (tput cols) - 8)",1,8)%s"'
     abbr --add show 'open -R'
 end
