6c9e3fd3ab19314cd2373afdd4003734bdcf1a55
Robert Böhnke <robb@robb.is>
2018-06-03 13:08:00 +0200

    Only add hombrew directory to PATH if it exists

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -9,15 +9,17 @@ set -x LANGUAGE "en_US.UTF-8"
 set -x LC_ALL "en_US.UTF-8"
 set -x LANG "en_US.UTF-8"
 
-# Homebrew
-set PATH "/usr/local/sbin" $PATH
-
 function update_profile --on-variable iterm2_profile
     echo -ne "\033]1337;SetProfile="$iterm2_profile"\a"
 end
 
 function fish_greeting
     echo -ne "\033]1337;SetProfile="$iterm2_profile"\a"
+end
+
+# Homebrew
+if test -d /usr/local/sbin
+    set PATH "/usr/local/sbin" $PATH
 end
 
 # Initialize RVM
