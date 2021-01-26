c2f8fb2bf49ed7cf8466eb46e9d65e17051ed8e5
Robert Böhnke <212465+robb@users.noreply.github.com>
2021-01-26 19:53:43 +0100

    Delete message.fish

--- fish/config/functions/message.fish
+++
@@ -1,6 +0,0 @@
-function message --argument-names from to message --description "Sends a message via iMessage.\nUsage: message sender recipient message"
-    echo "\
-        tell application \"Messages\"
-            send \"$message\" to buddy \"$to\" of service \"E:$from\"
-        end tell" | osascript > /dev/null
-end
